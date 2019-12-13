65c076b28d737a2221b6aacc8872eba50fd7f2b1
walterzhao <519362600@qq.com>
2019-12-13 09:09:10 +0000

    merge TestGit2

--- main.cpp
+++ main.cpp
@@ -1,44 +1,32 @@
 #include <iostream>
 using namespace std;
 
-void windows()
+void Bob()
 {
-	cout<<"This is windows Os"<<endl;
+	cout<<"I am Bob"<<endl;
 }
 
-void mac_master()
+void walter_mac()
 {
-    cout<<"This is mac master"<<endl;
+    cout<<"I am walter from mac"<<endl;
 }
 
-void windows_master()
+void bob_branch()
 {
-	cout<<"This is a windows_master"<<endl;
+	cout<<"This is a branch from Bob"<<endl;
 }
 
-void ppp()
+void mew_bob_branch()
 {
-	cout<<"ppp"<<endl;
-}
-
-void windows_branch()
-{
-	cout<<"This is a windows_branch"<<endl;
-}
-
-void mac_branch()
-{
-    cout<<"I am mac_branch"<<endl;
+	cout<<"I am new branch from bob"<<endl;
 }
 
 int main()
 {
     cout << "Hello, World!" <<endl;
-    windows();
-    mac_master();
-    windows_master();
- 	windows_branch();
- 	ppp();
-    mac_branch();
+    Bob();
+    walter_mac();
+    bob_branch();
+    new_bob_branch();
     return 0;
 }
